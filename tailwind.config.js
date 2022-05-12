7ac9524ef54e74ad5a7230b58e5174bc6712621a
Kojo Patrick <patrickasare84@gmail.com>
2022-05-12 20:05:00 +0000

    v1 not mobile friendly

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["*.{html,js}", "/src/**/*.{scss,js}"],
+	theme: {
+		extend: {
+			colors: {
+				"orange-red": "#FD5B2F",
+				"yellow-light": "#FFDB80",
+				"accent-green": "#13ded2",
+			},
+		},
+	},
+	plugins: [],
+};
